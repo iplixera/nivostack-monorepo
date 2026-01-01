2d062e40c0eb156e5d99e1446815defbd1f03a63
iplixera <iplixera@gmail.com>
2026-01-02 01:43:38 +0400

    fix: move publishing config inline to fix plugin resolution

--- packages/sdk-android/nivostack-core/build.gradle.kts
+++ packages/sdk-android/nivostack-core/build.gradle.kts
@@ -5,7 +5,46 @@ plugins {
 }
 
 // Publishing configuration
-apply(from = "publish.gradle.kts")
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                from(components["release"])
+                
+                groupId = "com.github.${project.findProperty("github.user") ?: "iplixera"}"
+                artifactId = "nivostack-android"
+                version = project.findProperty("version") as? String ?: "1.0.0"
+                
+                pom {
+                    name.set("NivoStack Android SDK")
+                    description.set("Android SDK for NivoStack - Mobile app monitoring and configuration platform")
+                    url.set("https://github.com/${project.findProperty("github.user") ?: "iplixera"}/nivostack-monorepo")
+                    
+                    licenses {
+                        license {
+                            name.set("MIT")
+                            url.set("https://opensource.org/licenses/MIT")
+                        }
+                    }
+                    
+                    developers {
+                        developer {
+                            id.set("nivostack")
+                            name.set("NivoStack Team")
+                            email.set("support@nivostack.com")
+                        }
+                    }
+                    
+                    scm {
+                        connection.set("scm:git:git://github.com/${project.findProperty("github.user") ?: "iplixera"}/nivostack-monorepo.git")
+                        developerConnection.set("scm:git:ssh://github.com:${project.findProperty("github.user") ?: "iplixera"}/nivostack-monorepo.git")
+                        url.set("https://github.com/${project.findProperty("github.user") ?: "iplixera"}/nivostack-monorepo/tree/main")
+                    }
+                }
+            }
+        }
+    }
+}
 
 android {
     namespace = "com.plixera.nivostack"
